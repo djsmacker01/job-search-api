34fc963368034b28d445c7b08ecd651e8beca3f0
Nurudeen Adedeji <djsmacker01@gmail.com>
2023-02-01 22:14:18 +0000

    add index.html file

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// const { response } = require('express');
 const express = require('express');
 
 const app = express();
@@ -13,6 +12,20 @@ app.get('/', (req, res) =>{
         hello: ' Hello world',  
     }
     res.json(data);
+
+})
+
+app.get('/info', (req,res)=>{
+    let info ={
+        last_name:'Adedeji',
+        first_name:'Adewale',
+        occupation: 'Full Stack Developer',
+        age:32
+    }
+    res.json(info);
+})
+
+app.post('/searchbycategory',(req,res)=>{
 
 })
 
@@ -49,75 +62,75 @@ app.listen(PORT, ()=>{
 //   console.log(`Server is up and listening on port ${PORT}`);
 // });
 
-// const data = [
-//   {
-//     id: "DC1793C1-EC81-D427-78BA-1312BEEFC4CC",
-//     jobTitle: "Platform engineer",
-//     company: "Magnis Dis Ltd",
-//     region: "Chattanooga",
-//     jobCategory: "IT",
-//   },
-//   {
-//     id: "0254383E-C856-5EE8-3B57-E2674EA0DEBF",
-//     jobTitle: "(user experience) designer",
-//     company: "Nulla Interdum Corporation",
-//     region: "Battagram",
-//     jobCategory: "IT",
-//   },
-//   {
-//     id: "6A38F873-23DB-3ED4-63AE-C6C117D3C22A",
-//     jobTitle: "assurance analyst",
-//     company: "Mauris Blandit Mattis Company",
-//     region: "San Venanzo",
-//     jobCategory: "IT",
-//   },
-//   {
-//     id: "5B513268-EE87-AA24-5817-0D66A4D3A331",
-//     jobTitle: "Software engineer",
-//     company: "Fusce Aliquam Institute",
-//     region: "Hallein",
-//     jobCategory: "IT",
-//   },
-//   {
-//     id: "AE5CAECD-FFBE-6931-2459-3BD478BE5294",
-//     jobTitle: "Software quality assurance",
-//     company: "Blandit Mattis Company",
-//     region: "Kalush",
-//     jobCategory: "IT",
-//   },
-//   {
-//     id: "DC1793C1-EC81-D427-78BA-1312BEEFC4CC",
-//     jobTitle: "Certified",
-//     company: "Magnis Dis Ltd",
-//     region: "Chattanooga",
-//     jobCategory: "Accounting",
-//   },
-//   {
-//     id: "0254383E-C856-5EE8-3B57-E2674EA0DEBF",
-//     jobTitle: "Financial",
-//     company: "Nulla Interdum Corporation",
-//     region: "Battagram",
-//     jobCategory: "Accounting",
-//   },
-//   {
-//     id: "6A38F873-23DB-3ED4-63AE-C6C117D3C22A",
-//     jobTitle: "Controller",
-//     company: "Mauris Blandit Mattis Company",
-//     region: "San Venanzo",
-//     jobCategory: "Accounting",
-//   },
-//   {
-//     id: "5B513268-EE87-AA24-5817-0D66A4D3A331",
-//     jobTitle: "Controller Accounting",
-//     company: "Fusce Aliquam Institute",
-//     region: "Hallein",
-//     jobCategory: "Accounting",
-//   },
-//   {
-//     id: "AE5CAECD-FFBE-6931-2459-3BD478BE5294",
-//     jobTitle: "clerk",
-//     company: "Blandit Mattis Company",
-//     region: "Kalush",
-//     jobCategory: "Accounting",
-//   },
-// ];
+const data = [
+  {
+    id: "DC1793C1-EC81-D427-78BA-1312BEEFC4CC",
+    jobTitle: "Platform engineer",
+    company: "Magnis Dis Ltd",
+    region: "Chattanooga",
+    jobCategory: "IT",
+  },
+  {
+    id: "0254383E-C856-5EE8-3B57-E2674EA0DEBF",
+    jobTitle: "(user experience) designer",
+    company: "Nulla Interdum Corporation",
+    region: "Battagram",
+    jobCategory: "IT",
+  },
+  {
+    id: "6A38F873-23DB-3ED4-63AE-C6C117D3C22A",
+    jobTitle: "assurance analyst",
+    company: "Mauris Blandit Mattis Company",
+    region: "San Venanzo",
+    jobCategory: "IT",
+  },
+  {
+    id: "5B513268-EE87-AA24-5817-0D66A4D3A331",
+    jobTitle: "Software engineer",
+    company: "Fusce Aliquam Institute",
+    region: "Hallein",
+    jobCategory: "IT",
+  },
+  {
+    id: "AE5CAECD-FFBE-6931-2459-3BD478BE5294",
+    jobTitle: "Software quality assurance",
+    company: "Blandit Mattis Company",
+    region: "Kalush",
+    jobCategory: "IT",
+  },
+  {
+    id: "DC1793C1-EC81-D427-78BA-1312BEEFC4CC",
+    jobTitle: "Certified",
+    company: "Magnis Dis Ltd",
+    region: "Chattanooga",
+    jobCategory: "Accounting",
+  },
+  {
+    id: "0254383E-C856-5EE8-3B57-E2674EA0DEBF",
+    jobTitle: "Financial",
+    company: "Nulla Interdum Corporation",
+    region: "Battagram",
+    jobCategory: "Accounting",
+  },
+  {
+    id: "6A38F873-23DB-3ED4-63AE-C6C117D3C22A",
+    jobTitle: "Controller",
+    company: "Mauris Blandit Mattis Company",
+    region: "San Venanzo",
+    jobCategory: "Accounting",
+  },
+  {
+    id: "5B513268-EE87-AA24-5817-0D66A4D3A331",
+    jobTitle: "Controller Accounting",
+    company: "Fusce Aliquam Institute",
+    region: "Hallein",
+    jobCategory: "Accounting",
+  },
+  {
+    id: "AE5CAECD-FFBE-6931-2459-3BD478BE5294",
+    jobTitle: "clerk",
+    company: "Blandit Mattis Company",
+    region: "Kalush",
+    jobCategory: "Accounting",
+  },
+];
